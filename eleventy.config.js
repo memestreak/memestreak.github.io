2d1a986966e1707f9ff3eaab1ecd12269a029f82
Jeremy Ellington <jme@eminor.net>
2024-12-11 07:16:49 -0600

    Initial checkin

---
+++ eleventy.config.js
@@ -0,0 +1,41 @@
+// See: www.11ty.dev/docs/config/
+
+// See: github.com/kentaroi/eleventy-sass
+const eleventySass = require("eleventy-sass");
+
+//
+// Main entry point
+//
+module.exports = async function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventySass);
+
+    return {
+        dir: {
+            // Controls the top level directory/file/glob that we’ll
+            // use to look for templates.
+            input: "src",
+
+            // The includes directory is meant for Eleventy layouts,
+            // include files, extends files, partials, or
+            // macros. These files will not be processed as full
+            // template files, but can be consumed by other templates.
+            includes: "layouts/partials",
+
+            // This configuration option is optional but useful if you
+            // want your Eleventy layouts to live outside of the
+            // Includes directory. Just like the Includes directory,
+            // these files will not be processed as full template
+            // files, but can be consumed by other templates.
+      	    layouts: "layouts",
+
+            // Controls the directory inside which the global data
+            // template files, available to all templates, can be
+            // found. See www.11ty.dev/docs/data-global.
+            data: "data",
+
+            // Controls the directory inside which the finished
+            // templates will be written to.
+            output: "_site",
+        },
+    };
+};
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: This is a title from front matter
+layout: base.njk
+---
+
+# This is h1 header in index.md.
+
+This is markdown content.
